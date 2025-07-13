80b8dc8caea392362596a50efa9fee9bf9ae65fc
username / Alessandro Mandolito <https://github.com/alexmando/ingw-es1.git>
2025-07-13 21:13:39 +0200

    ristorante

--- src/main/java/Controller/AuthController.java
+++ src/main/java/Controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
